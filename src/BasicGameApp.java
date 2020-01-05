bb5d108d3549f6beed39a8a261f5a061890450b5
Tommy Gultiano Hansen <54972963+EWCMF@users.noreply.github.com>
2020-01-05 23:25:36 +0100

    Uses tmx now.

--- src/BasicGameApp.java
+++ src/BasicGameApp.java
@@ -4,23 +4,13 @@ import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.app.Viewport;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
-import com.almasb.fxgl.entity.components.CollidableComponent;
 import com.almasb.fxgl.input.Input;
 import com.almasb.fxgl.input.UserAction;
-import com.almasb.fxgl.physics.BoundingShape;
 import com.almasb.fxgl.physics.CollisionHandler;
-import com.almasb.fxgl.physics.HitBox;
-import com.almasb.fxgl.physics.PhysicsComponent;
-import com.almasb.fxgl.physics.box2d.dynamics.BodyType;
-import com.almasb.fxgl.physics.box2d.dynamics.FixtureDef;
 import components.PlayerComponent;
 import javafx.geometry.Point2D;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
 import java.util.Map;
 
 
@@ -29,7 +19,7 @@ public class BasicGameApp extends GameApplication {
     private double aimDownVectorY = 150;
 
     public enum EntityType {
-        PLAYER, TARGET, WALL, BULLET, EXIT
+        PLAYER, TARGET, WALL, BULLET, START, EXIT
     }
 
     @Override
@@ -48,7 +38,6 @@ public class BasicGameApp extends GameApplication {
             @Override
             protected void onAction() {
                 player.getComponent(PlayerComponent.class).right();
-                FXGL.getGameState().increment("pixelsMoved", +5);
             }
 
             @Override
@@ -61,7 +50,6 @@ public class BasicGameApp extends GameApplication {
             @Override
             protected void onAction() {
                 player.getComponent(PlayerComponent.class).left();
-                FXGL.getGameState().increment("pixelsMoved", +5);
             }
 
             @Override
@@ -74,11 +62,10 @@ public class BasicGameApp extends GameApplication {
             @Override
             protected void onActionBegin() {
                 player.getComponent(PlayerComponent.class).jump();
-                FXGL.getGameState().increment("pixelsMoved", +5);
             }
         }, KeyCode.SPACE);
 
-        input.addAction(new UserAction("Play Sound") {
+        input.addAction(new UserAction("Test vectors") {
             @Override
             protected void onActionBegin() {
                 System.out.println(player.getPosition());
@@ -123,94 +110,28 @@ public class BasicGameApp extends GameApplication {
 
     @Override
     protected void initGameVars(Map<String, Object> vars) {
-        vars.put("pixelsMoved", 0);
     }
 
     public Entity player;
+    public Point2D start;
 
     @Override
     protected void initGame() {
         FXGL.getGameWorld().addEntityFactory(new BasicGameFactory());
+        FXGL.setLevelFromMap("test.tmx");
 
-        PhysicsComponent physicsComponent = new PhysicsComponent();
-        physicsComponent.setBodyType(BodyType.DYNAMIC);
-        physicsComponent.addGroundSensor(new HitBox(BoundingShape.box(64, 94)));
+        start = FXGL.getGameWorld().getSingleton(EntityType.START).getPosition();
 
-        physicsComponent.setFixtureDef(new FixtureDef().friction(0.0f));
-
-        player = FXGL.entityBuilder()
-                .type(EntityType.PLAYER)
-                .at(300, 300)
-                //.view(new Rectangle(25, 25, Color.RED))
-                .bbox(new HitBox(BoundingShape.box(64, 94)))
-                .with(physicsComponent)
-                .with(new CollidableComponent(true))
-                .with(new PlayerComponent())
-                .buildAndAttach();
-
-        FXGL.entityBuilder()
-                .type(EntityType.TARGET)
-                .at(500, 200)
-                .viewWithBBox(new Circle(15, Color.BLACK))
-                .with(new CollidableComponent(true))
-                .buildAndAttach();
-
-        FXGL.entityBuilder()
-                .type(EntityType.TARGET)
-                .at(100, 200)
-                .viewWithBBox(new Circle(15, Color.BLACK))
-                .with(new CollidableComponent(true))
-                .buildAndAttach();
-
-        FXGL.entityBuilder()
-                .type(EntityType.WALL)
-                .at(0, 670)
-                .view(new Rectangle(600 * 5, 50, Color.DARKGREY))
-                .bbox(new HitBox(BoundingShape.box(600 * 5, 50)))
-                .with(new PhysicsComponent())
-                .with(new CollidableComponent(true))
-                .buildAndAttach();
-
-        FXGL.entityBuilder()
-                .type(EntityType.WALL)
-                .at(-50, 0)
-                .view(new Rectangle(50, 720, Color.DARKGREY))
-                .bbox(new HitBox(BoundingShape.box(50, 600)))
-                .with(new PhysicsComponent())
-                .with(new CollidableComponent(true))
-                .buildAndAttach();
-
-        FXGL.entityBuilder()
-                .type(EntityType.WALL)
-                .at(600, 520)
-                .view(new Rectangle(50, 200, Color.DARKGREY))
-                .bbox(new HitBox(BoundingShape.box(50, 200)))
-                .with(new PhysicsComponent())
-                .with(new CollidableComponent(true))
-                .buildAndAttach();
-
-        FXGL.entityBuilder()
-                .type(EntityType.EXIT)
-                .at(600 * 5 - 200, 520)
-                .view(new Rectangle(100, 150, Color.GREEN))
-                .with(new CollidableComponent(true))
-                .buildAndAttach();
+        player = FXGL.getGameWorld().spawn("player", start);
 
         Viewport viewport = FXGL.getGameScene().getViewport();
 
-        viewport.setBounds(0, 0, 600 * 5, 720);
+        viewport.setBounds(21, 0, 21 * 100, 21 * 35);
         viewport.bindToEntity(player, FXGL.getAppWidth() / 3, FXGL.getAppHeight() / 2);
     }
 
     @Override
     protected void initUI() {
-        Text textPixels = new Text();
-        textPixels.setTranslateX(100); // x = 50
-        textPixels.setTranslateY(100); // y = 100
-
-        textPixels.textProperty().bind(FXGL.getGameState().intProperty("pixelsMoved").asString());
-
-        FXGL.getGameScene().addUINode(textPixels); // add to the scene graph
     }
 
     @Override
--- src/BasicGameFactory.java
+++ src/BasicGameFactory.java
@@ -8,6 +8,10 @@ import com.almasb.fxgl.entity.Spawns;
 import com.almasb.fxgl.entity.components.CollidableComponent;
 import com.almasb.fxgl.physics.BoundingShape;
 import com.almasb.fxgl.physics.HitBox;
+import com.almasb.fxgl.physics.PhysicsComponent;
+import com.almasb.fxgl.physics.box2d.dynamics.BodyType;
+import com.almasb.fxgl.physics.box2d.dynamics.FixtureDef;
+import components.PlayerComponent;
 import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -29,4 +33,50 @@ public class BasicGameFactory implements EntityFactory {
                 .build();
     }
 
+    @Spawns("start")
+    public Entity newStart(SpawnData data) {
+        return entityBuilder()
+                .type(BasicGameApp.EntityType.START)
+                .from(data)
+                .build();
+    }
+
+    @Spawns("player")
+    public Entity newPlayer(SpawnData data) {
+        PhysicsComponent physicsComponent = new PhysicsComponent();
+        physicsComponent.setBodyType(BodyType.DYNAMIC);
+        physicsComponent.addGroundSensor(new HitBox(BoundingShape.box(48, 94)));
+
+        physicsComponent.setFixtureDef(new FixtureDef().friction(0.0f));
+        return entityBuilder()
+                .type(BasicGameApp.EntityType.PLAYER)
+                .from(data)
+                //.view(new Rectangle(25, 25, Color.RED))
+                .bbox(new HitBox(BoundingShape.box(48, 94)))
+                .with(physicsComponent)
+                .with(new CollidableComponent(true))
+                .with(new PlayerComponent())
+                .build();
+    }
+
+    @Spawns("platform")
+    public Entity newPlatform(SpawnData data) {
+        return entityBuilder()
+                .type(BasicGameApp.EntityType.WALL)
+                .from(data)
+                .bbox(new HitBox(BoundingShape.box(data.<Integer>get("width"), data.<Integer>get("height"))))
+                .with(new PhysicsComponent())
+                .with(new CollidableComponent(true))
+                .build();
+    }
+
+    @Spawns("target")
+    public Entity newTarget(SpawnData data) {
+        return entityBuilder()
+                .type(BasicGameApp.EntityType.TARGET)
+                .from(data)
+                .viewWithBBox(new Circle(15, Color.BLACK))
+                .with(new CollidableComponent(true))
+                .build();
+    }
 }
